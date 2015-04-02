a979479e4ec7684b0209356f2189298b1158083b
Ricardo <ricardo.t.ramirez@gmail.com>
2015-04-02 10:10:14 -0400

    Function to confer f to c

--- src/temp_convert.py
+++ src/temp_convert.py
@@ -2,4 +2,9 @@ def f_to_k(temp):
 	return((temp-32)*5.0/9) + 273.15
 
 def k_to_c(temp):
-	something
+	return temp -273.15
+
+def f_to_c(temp):
+	temp_k = f_to_k(temp)
+	result = k_to_c(temp_k)
+	return(result)
